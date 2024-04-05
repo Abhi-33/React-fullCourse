277c95937da223af2efb6126e3d9499e92c85b8f
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-05 16:13:45 +0530

    Updated all the files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 
 function App() {
+  const [mode ,setMode ] =  useState('light') //it tells whether the dark mode is enabled or not 
   return (
    <>
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode}/>
 <div className="container my-3">
 <TextForm heading="Enter the Text to analyze Below" />
 {/* <About /> */}
