f41316bb6d064af43ef831ca8c77c78e127f27d3
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-07 16:06:50 +0530

    Learning : Router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
 import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';/
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); //it tells whether the dark mode is enabled or not 
-  const [alert, setAlert] = useState(null);
+  const [mode ,setMode ] =  useState('light'); //it tells whether the dark mode is enabled or not 
+  const [alert , setAlert] = useState(null);
 
-  const showAlert = (message, type) => {
+  const showAlert = (message , type)=>{
     setAlert({
-      msg: message,
-      type: type
+      msg : message,
+      type : type
     })
     setTimeout(() => {
       setAlert(null);
     }, 1500);
   }
-
-  const toggleMode = () => {
+  
+  const toggleMode = ()=>{
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#212529';
-      showAlert("Dark Mode Has Been Enabled", "success");
+      showAlert("Dark Mode Has Been Enabled" , "success");
       document.title = 'TextUtils - Dark Mode';
-
+     
       // Bad Practice - "Chapri Virus Websites Code"
       // setInterval(() => {
       //   document.title = 'TextUtils is Amazing';
@@ -40,29 +34,22 @@ function App() {
       //   document.title = 'Install TextUtils Now!';
       // }, 1500);
     }
-    else {
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert("Light Mode Has Been Enabled", "success");
-      document.title = 'TextUtils - Light Mode';
-    }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert("Light Mode Has Been Enabled" , "success");
+    document.title = 'TextUtils - Light Mode';
+  }
   }
   return (
-    <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <TextForm showAlert={showAlert} heading="Enter the Text to analyze Below" mode={mode} />
-          </Route>
-        </Switch>
-        {/* <About /> */}
-      </div>
-    </>
+   <>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert alert = {alert}/>
+<div className="container my-3">
+<TextForm showAlert={showAlert} heading="Enter the Text to analyze Below"   mode={mode} />
+{/* <About /> */}
+</div>
+   </>
   );
 }
 
